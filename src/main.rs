6f9425807bf9ec9e49a052168d621d2804f3afeb
John Higgins <johnmatthiggins@gmail.com>
2022-08-13 01:08:58 -0700

    Got parser working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::vec::Vec;
+use std::env;
 
+#[derive(Debug)]
 struct CronExp {
     minute: ExpSeg,
     hour: ExpSeg,
@@ -9,76 +11,153 @@ struct CronExp {
 }
 
 // Segment of CRON expression.
+#[derive(Debug, Clone)]
 enum ExpSeg {
     List(Vec<usize>),
-    Range(String, String),
+    Range(usize, usize),
     Frac(usize),
     Symbol(CronSymbol),
 }
 
+#[derive(Debug, Clone)]
 enum CronSymbol {
     Wildcard,
     Number(usize),
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("USAGE: cronhint '* * * * *'");
+    }
+    else {
+        let cron_str = args
+            .get(1)
+            .unwrap()
+            .to_string();
+
+        let cron_exp = parse_cron_exp(&cron_str);
+
+        println!("{}", cron_exp.unwrap().to_string());
+    }
+}
+
+impl ToString for CronExp {
+    fn to_string(&self) -> String {
+        String::from(format!(
+                     "{} {} {} {} {}",
+                     self.minute.to_string(),
+                     self.hour.to_string(),
+                     self.day.to_string(),
+                     self.month.to_string(),
+                     self.weekday.to_string()))
+    }
+}
+
+impl ToString for ExpSeg {
+    fn to_string(&self) -> String {
+        match self {
+            ExpSeg::List(l) => l.into_iter().map(|x| x.to_string()).collect::<Vec<String>>().join(","),
+            ExpSeg::Range(a, b) => String::from(format!("{}-{}", a, b)),
+            ExpSeg::Frac(v) => String::from(format!("*/{}", v)),
+            ExpSeg::Symbol(s) => match s {
+                CronSymbol::Wildcard => String::from("*"),
+                CronSymbol::Number(n) => n.to_string(),
+            },
+        }
+    }
+}
+
+fn parse_cron_exp(cron_exp: &String) -> Option<CronExp> {
+    let cron_vec: Vec<&str> = cron_exp
+        .split(" ")
+        .collect();
+
+    if cron_vec.len() == 5 {
+        let cron_segs: Vec<ExpSeg> = cron_vec
+            .into_iter()
+            .map(|x| parse_exp_seg(&x.to_string()))
+            .filter(|x| matches!(x, Some(_)))
+            .map(|x| x.unwrap())
+            .collect();
+
+        if cron_segs.len() == 5 {
+            let result = CronExp {
+                minute: cron_segs.get(0).unwrap().clone(),
+                hour: cron_segs.get(1).unwrap().clone(),
+                day: cron_segs.get(2).unwrap().clone(),
+                month: cron_segs.get(3).unwrap().clone(),
+                weekday: cron_segs.get(4).unwrap().clone(),
+            };
+
+            Some(result)
+        }
+        else {
+            None
+        }
+    }
+    else {
+        None
+    }
+}
+
+fn parse_exp_seg(exp: &String) -> Option<ExpSeg> {
+    if let Some(v) = parse_list(exp) {
+        Some(v)
+    }
+    else if let Some(v) = parse_range(exp) {
+        Some(v)
+    }
+    else if let Some(v) = parse_sym(exp) {
+        Some(v)
+    }
+    else if let Some(v) = parse_frac(exp) {
+        Some(v)
+    }
+    else {
+        None
+    }
 }
 
 fn parse_list(exp: &String) -> Option<ExpSeg> {
-    let mut fail = false;
-    let chars: Vec<char> = exp.chars().collect();
-    let mut output: Vec<usize> = Vec::new();
-    let mut buffer: Vec<char> = Vec::new();
+    let list: Vec<&str> = exp
+        .split(",")
+        .collect();
 
-    let limit: usize = chars.len();
-    let mut i = 0;
+    let result: Vec<usize> = list
+        .clone()
+        .into_iter()
+        .map(|x| x.parse::<usize>().ok())
+        .filter(|x| matches!(x, Some(_)))
+        .map(|x| x.unwrap())
+        .collect();
 
-    while i < limit && !fail {
-        let c = chars.get(i).unwrap();
-
-        if c == &',' {
-            if buffer.is_empty() {
-                fail = true;
-            }
-            else {
-                let num_str: String = buffer
-                    .clone()
-                    .into_iter()
-                    .collect();
-                buffer.clear();
-
-                let num = num_str.parse::<usize>();
-
-                if let Ok(n) = num {
-                    output.push(n);
-                }
-                else {
-                    fail = true;
-                }
-            }
-        }
-        else if c.is_digit(10) {
-            buffer.push(*c);
-        }
-        else {
-            fail = true;
-        }
-    }
-
-    if fail {
+    if result.len() != list.len() {
         None
     }
     else {
-        Some(ExpSeg::List(output))
+        Some(ExpSeg::List(result))
     }
 }
 
 fn parse_range(exp: &String) -> Option<ExpSeg> {
-    None
+    let split_exp: Vec<&str> = exp.split("-").collect();
+
+    let first: Option<usize> = split_exp
+        .get(0)
+        .and_then(|x| x.parse::<usize>().ok());
+    let second: Option<usize> = split_exp
+        .get(1)
+        .and_then(|x| x.parse::<usize>().ok());
+
+    // Combine into range expression.
+    first.and_then(|a| second
+            .map(|b| ExpSeg::Range(a, b)))
 }
 
 fn parse_frac(exp: &String) -> Option<ExpSeg> {
-    let mut split_exp: Vec<&str> = exp.split("/").collect();
+    let split_exp: Vec<&str> = exp.split("/").collect();
 
     if split_exp.first().map_or(false, |x| *x == "*") {
         split_exp
